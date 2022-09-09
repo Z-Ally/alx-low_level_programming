46b026c5cfa336237830a5136cfa371c947a3d45
Z-Ally <Z-Ally>
2022-09-09 16:19:54 -0700

    C0x01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,18 @@
  */
 int main(void)
 {
-	int n;	
+	int n;
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;	
+
+	n = rand() - RAND_MAX / 2;
 	/*my code*/
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-	{	
-			printf("%d is zero\n", n);
+	{
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
