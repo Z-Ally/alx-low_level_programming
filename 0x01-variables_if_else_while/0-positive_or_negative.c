22f7249fb3f346b29b29c4fdeac85d83645cb6c4
Z-Ally <Z-Ally>
2022-09-09 16:11:23 -0700

    betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -20,12 +19,17 @@ int main(void)
 	
 	/*my code*/
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
+	{	
+			printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
-	
+	}
 	return (0);
 }
 
