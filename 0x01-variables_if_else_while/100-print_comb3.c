f7651abdc2388c57669d1a3a283e8d9f4f101dfd
Z-Ally <Z-Ally>
2022-09-09 19:34:49 -0700

    c0x01

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,12 @@
 #include<stdio.h>
 
 /**
-
  * main - Entry point
-
  *
-
  * Description: print all possible different
-
  *           combinations of two digits.
-
  *
-
  * Return: Always 0 (Success)
-
 */
 
 int main(void)
@@ -24,15 +17,11 @@ int main(void)
 	{
 		digit2 = 0;
           	while (digit2 <= 9)
-
 		{
-
 			if (digit1 != digit2 && digit1 < digit2)
-
 			{
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
-
 				if (digit1 + digit2 != 17)
 				{
 					putchar(',');
@@ -44,6 +33,5 @@ int main(void)
 		++digit1;
 	}
 	putchar('\n');
-
 	return (0);
 }
