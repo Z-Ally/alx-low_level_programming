10a9fde44b30f585d0d9b1ccc2b791e951b76a9f
Z-Ally <Z-Ally>
2022-09-16 03:55:27 -0700

    c0x04

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,15 @@
 /**
  * print_numbers - print numbers
  * Return: Always 0.
+ *
  */
+
 void print_numbers(void)
 {
 	int i;
 
-	int = 0;
+	char i = 0;
+
 	while (i < 10)
 	{
 		_putchar (i + '0');
